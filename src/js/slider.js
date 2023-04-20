7aa7819a1c63cc870a29eb52cef07f75c0b7c1da
Vadym <kimvadik@gmail.com>
2023-04-21 00:50:55 +0300

    add fav

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,9 +1,21 @@
   import Swiper from 'swiper/bundle';
 
-    const swiper = new Swiper('.swiper', {
-        loop: true, longSwipesRatio: 0.5,
-          pagination: {
-    el: '.slider-pagination',
-    type: 'bullets',
-  },
-  });
+  //   const swiper = new Swiper('.swiper', {
+  //       loop: true, longSwipesRatio: 0.5,
+  //         pagination: {
+  //   el: '.slider-pagination',
+  //   type: 'bullets',
+  // },
+  //   });
+  const swiper = new Swiper(".mySwiper", {
+        loop: true,
+        spaceBetween: 10,
+        slidesPerView: 1,
+        navigation: {
+          nextEl: ".button-next",
+          prevEl: ".button-prev",
+        },
+        mousewheel: {
+          invert: true,
+        },
+      });
